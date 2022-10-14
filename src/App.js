3d4cbeaf7fb159c126b0dfb2c1e2b86854fb6ed7
Kesara Wimal <kesarawimal9@gmail.com>
2022-10-14 16:29:01 +0530

    design changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {contractETH, contractMATIC} from './contract';
 import './App.css';
 import React, {Component} from "react";
+import {Input, Button, Container, Card, Grid, Header, Icon, Menu} from 'semantic-ui-react';
 import namehash from "./namehash";
 
 class App extends Component {
-    state = {domain: '', data: []};
+    state = {domain: '', data: [], loading: false};
 
     onSubmit = async (event) => {
         event.preventDefault();
+        this.setState({loading: true});
 
         const interestedKeys = [
             "ipfs.html.value",
@@ -24,36 +26,138 @@ class App extends Component {
         }
 
         this.setState({data: data});
+        this.setState({loading: false});
     };
 
     showResults() {
         if (this.state.data.length > 0) {
+            let items = [];
+
             if (this.state.data['owner'] === "0x0000000000000000000000000000000000000000") {
-                return <p>Not Available!</p>;
+                const item = {};
+                item.header = 'Not Available!';
+                item.description = 'Not found!';
+
+                items.push(item);
             } else {
-                return (
-                    <div>
-                        <p><a target="_blank" href={"https://dweb.link/ipfs/" + this.state.data[2][0]}>Visit Website</a></p>
-                        <p>Domain Owner: {this.state.data['owner']}</p>
-                        <p>Domain Resolver: {this.state.data['resolver']}</p>
-                        <p>BTC Address: {this.state.data[2][1]}</p>
-                        <p>ETH Address: {this.state.data[2][2]}</p>
-                        <p>ADA Address: {this.state.data[2][3]}</p>
-                        <p>MATIC Address: {this.state.data[2][4]}</p>
-                    </div>
-                );
+                if (this.state.data[2][0] !== "") {
+                    const item = {};
+                    item.header = 'Website';
+                    item.description = 'https://dweb.link/ipfs/' + this.state.data[2][0];
+                    item.href = 'https://dweb.link/ipfs/' + this.state.data[2][0];
+
+                    items.push(item);
+                }
+
+                if (this.state.data['owner'] !== "0x0000000000000000000000000000000000000000") {
+                    const item = {};
+                    item.header = 'Owner';
+                    item.description = this.state.data['owner'];
+
+                    items.push(item);
+                }
+
+                if (this.state.data['resolver'] !== "0x0000000000000000000000000000000000000000") {
+                    const item = {};
+                    item.header = 'Resolver';
+                    item.description = this.state.data['resolver'];
+
+                    items.push(item);
+                }
+
+                if (this.state.data[2][1] !== "") {
+                    const item = {};
+                    item.header = 'BTC Address';
+                    item.description = this.state.data[2][1];
+
+                    items.push(item);
+                }
+
+                if (this.state.data[2][2] !== "") {
+                    const item = {};
+                    item.header = 'ETH Address';
+                    item.description = this.state.data[2][2];
+
+                    items.push(item);
+                }
+
+                if (this.state.data[2][3] !== "") {
+                    const item = {};
+                    item.header = 'ADA Address';
+                    item.description = this.state.data[2][3];
+
+                    items.push(item);
+                }
+
+                if (this.state.data[2][4] !== "") {
+                    const item = {};
+                    item.header = 'MATIC Address';
+                    item.description = this.state.data[2][4];
+
+                    items.push(item);
+                }
             }
+
+            return (
+                <Card.Content>
+                    <Card.Group itemsPerRow={1} centered items={items}/>
+                </Card.Content>
+            );
         }
     }
 
     render() {
         return (
-            <div className="App">
-                <form onSubmit={this.onSubmit}>
-                    <input onChange={event => this.setState({domain: event.target.value})}/>
-                    <button type="submit">Resolve</button>
-                </form>
-                {this.showResults()}
+            <div className='App'>
+                <Container>
+                    <Grid centered>
+                        <Grid.Row>
+                            <Grid.Column width={12}>
+                                <Card fluid>
+                                    <Card.Content>
+                                        <Card.Header>
+                                            <Menu>
+                                                <Menu.Item href='/'>
+                                                    <Header as='h2'>
+                                                        <Icon name='cogs'/>
+                                                        <Header.Content>
+                                                            Unstoppable Domains Resolver
+                                                            <Header.Subheader>Resolve any NFT domain to view wrapped
+                                                                content</Header.Subheader>
+                                                        </Header.Content>
+                                                    </Header>
+                                                </Menu.Item>
+
+                                                <Menu.Menu position='right'>
+                                                    <Menu.Item
+                                                        href='https://github.com/kesarawimal/unstoppable-domains-resolver'>
+                                                        <Button secondary icon='github' content='Repository'/>
+                                                    </Menu.Item>
+                                                </Menu.Menu>
+                                            </Menu>
+
+                                        </Card.Header>
+                                    </Card.Content>
+                                    <Card.Content>
+                                        <Grid centered>
+                                            <Grid.Row>
+                                                <form onSubmit={this.onSubmit}>
+                                                    <Input size='huge' type='text' placeholder='Search for NFT domains'
+                                                           action>
+                                                        <input
+                                                            onChange={event => this.setState({domain: event.target.value})}/>
+                                                        <Button loading={this.state.loading} size='big' secondary type='submit'>Resolve</Button>
+                                                    </Input>
+                                                </form>
+                                            </Grid.Row>
+                                        </Grid>
+                                    </Card.Content>
+                                    {this.showResults()}
+                                </Card>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
             </div>
         );
     }
